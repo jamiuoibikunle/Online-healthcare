43d0b34e015379dc871064496954f544dcbd4674
Ibikunle Jamiu <ibidotun@gmail.com>
2022-07-12 23:15:52 +0100

    Fixed the home bug

--- src/Components/Home/Homepage.jsx
+++ src/Components/Home/Homepage.jsx
@@ -31,38 +31,38 @@ const Homepage = () => {
 
     getName()
 
-	useEffect(() => {
+	// useEffect(() => {
 
-		const getTime = () => {
+	// 	const getTime = () => {
 
-			const hour = new Date().getHours()
-            // const hour = 10
+	// 		const hour = new Date().getHours()
+    //         // const hour = 10
 
-			if ( hour > 0 && hour < 12 ) {
-				setGreetings('Good morning')
-			} else if ( hour > 11 && hour < 16 ) {
-				setGreetings('Good afternoon')
-			} else {
-				setGreetings('Good evening')
-			}
+	// 		if ( hour > 0 && hour < 12 ) {
+	// 			setGreetings('Good morning')
+	// 		} else if ( hour > 11 && hour < 16 ) {
+	// 			setGreetings('Good afternoon')
+	// 		} else {
+	// 			setGreetings('Good evening')
+	// 		}
 
-			// if ( 12 > hour > 0 ) setGreetings('Good morning')
-			// if ( 16 > hour > 11 ) setGreetings('Good afternoon')
-			// if ( 23  hour > 11 ) setGreetings('Good afternoon')
-			// else setGreetings('Good evening')
+	// 		// if ( 12 > hour > 0 ) setGreetings('Good morning')
+	// 		// if ( 16 > hour > 11 ) setGreetings('Good afternoon')
+	// 		// if ( 23  hour > 11 ) setGreetings('Good afternoon')
+	// 		// else setGreetings('Good evening')
 
-		}
+	// 	}
 
-		getTime()
+	// 	getTime()
 
-	}, [ user ])
+	// }, [ user ])
 
-	// return details.name ? (
-        return (
+	return context.userDetails ? (
+        // return (
 		<div className={styles.homewrapper}>
 			<div className={styles.header}>
 				<div className={styles.greeting}>
-					{greetings}, {context.userDetails.name}
+					Hello, {context.userDetails.name}
 				</div>
 				<div className={styles.welcome}>
 					Welcome back
@@ -78,9 +78,9 @@ const Homepage = () => {
 			</div>
 		</div>
 	) 
-    // : (
-	// 		<CircularProgress className={styles.progress} />
-	// )
+    : (
+			<CircularProgress className={styles.progress} />
+	)
 }
 
 export default Homepage
